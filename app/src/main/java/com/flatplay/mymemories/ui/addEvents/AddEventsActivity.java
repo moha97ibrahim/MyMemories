2cead2ecf1cd5a97e3f0935fe65c85e1155e756e
moha97brahim <moha97ibrahim@gmail.com>
2020-02-01 00:32:17 +0530

    initial commit

--- app/src/main/java/com/flatplay/mymemories/ui/addEvents/AddEventsActivity.java
+++ app/src/main/java/com/flatplay/mymemories/ui/addEvents/AddEventsActivity.java
@@ -1,17 +1,167 @@
 package com.flatplay.mymemories.ui.addEvents;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
+import android.content.ContentValues;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.flatplay.mymemories.MainActivity;
 import com.flatplay.mymemories.R;
+import com.flatplay.mymemories.db.DBContract;
+import com.google.android.material.snackbar.Snackbar;
+
 
 public class AddEventsActivity extends AppCompatActivity {
+    private EditText eventTitle, eventSubject, eventBody;
+    private CheckBox everyDay, monthly, yearly;
+    private Button cancel, save;
+    private String title, subject, body, date;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_addevents);
 
+
+        eventTitle = findViewById(R.id.event_title);
+        eventSubject = findViewById(R.id.event_subject);
+        eventBody = findViewById(R.id.event_body);
+        everyDay = findViewById(R.id.checkEveryDay);
+        monthly = findViewById(R.id.checkEveryMonth);
+        yearly = findViewById(R.id.checkEveryYear);
+        cancel = findViewById(R.id.btn_cancel);
+        save = findViewById(R.id.btn_save);
+
+        //toolbar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Add Event");
+
+        Toast.makeText(AddEventsActivity.this, "" + getIntent().getStringExtra("DATE"), Toast.LENGTH_LONG).show();
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cancelEvent();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveEvent();
+            }
+        });
+
+        everyDay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                monthly.setChecked(false);
+                yearly.setChecked(false);
+            }
+        });
+
+        monthly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                everyDay.setChecked(false);
+                yearly.setChecked(false);
+            }
+        });
+
+        yearly.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                monthly.setChecked(false);
+                everyDay.setChecked(false);
+            }
+        });
+
     }
+
+    private void cancelEvent() {
+        Intent intent = new Intent(AddEventsActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void saveEvent() {
+        if (checkRequiredField() && checkEventStatus()) {
+            ContentValues values = new ContentValues();
+            values.put(DBContract.event.COLUMN_EVENT_DATE, getDate());
+            values.put(DBContract.event.COLUMN_EVENT_TITLE, title);
+            values.put(DBContract.event.COLUMN_EVENT_SUBJECT, subject);
+            values.put(DBContract.event.COLUMN_EVENT_BODY, body);
+            values.put(DBContract.event.COLUMN_EVENT_STATUS, getStatus());
+            Uri newUri = getContentResolver().insert(DBContract.event.CONTENT_URI, values);
+
+            if (newUri == null) {
+                Toast.makeText(this, "Profile Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Profile Saved", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(AddEventsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }
+    }
+
+    private String getDate() {
+        if (date == null) {
+            return date = getCurrentDate();
+        } else {
+            return date;
+        }
+    }
+
+    private String getCurrentDate() {
+        return null;
+    }
+
+
+    private String getStatus() {
+        if (everyDay.isChecked()) {
+            return "DAY";
+        } else if (monthly.isChecked()) {
+            return "MONTH";
+        } else if (yearly.isChecked()) {
+            return "YEAR";
+        } else {
+            return null;
+        }
+    }
+
+
+    private boolean checkRequiredField() {
+        title = eventTitle.getText().toString().trim();
+        subject = eventSubject.getText().toString().trim();
+        body = eventBody.getText().toString().trim();
+        if (TextUtils.isEmpty(title) || TextUtils.isEmpty(subject) || TextUtils.isEmpty(body)) {
+            Toast.makeText(AddEventsActivity.this, "Required Field Empty", Toast.LENGTH_LONG).show();
+            return false;
+        } else {
+            return true;
+        }
+
+    }
+
+    private boolean checkEventStatus() {
+        if ((yearly.isChecked()) || (monthly.isChecked()) || (everyDay.isChecked())) {
+            return true;
+        } else {
+            Toast.makeText(AddEventsActivity.this, "Choose any event type", Toast.LENGTH_LONG).show();
+            return false;
+        }
+    }
+
 }
--- app/src/main/java/com/flatplay/mymemories/ui/home/WriteFragment.java
+++ app/src/main/java/com/flatplay/mymemories/ui/home/WriteFragment.java
@@ -9,20 +9,32 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.flatplay.mymemories.R;
 import com.flatplay.mymemories.ui.addEvents.AddEventsActivity;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class WriteFragment extends Fragment {
 
     private WriteViewModel writeViewModel;
     private FloatingActionButton addEventfab;
+    private CompactCalendarView compactCalendarView;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMM -yyyy", Locale.getDefault());
+    private String date;
+    private ActionBar actionBar;
+    private AppCompatActivity activity;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,14 +49,43 @@ public class WriteFragment extends Fragment {
             }
         });
 
+        //fab
         addEventfab = root.findViewById(R.id.add_event_fab);
         addEventfab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), AddEventsActivity.class);
+                intent.putExtra("DATE",date);
                 startActivity(intent);
+                getActivity().finish();
             }
         });
+
+        //calendar
+        compactCalendarView = root.findViewById(R.id.compactcalendar_view);
+        //toolbar
+        activity = (AppCompatActivity) getActivity();
+        actionBar = activity.getSupportActionBar();
+        compactCalendarView.setUseThreeLetterAbbreviation(true);
+        compactCalendarView.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                date = getDate(dateClicked.getTime());
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                actionBar.setTitle(simpleDateFormat.format(firstDayOfNewMonth));
+            }
+        });
+
         return root;
+    }
+
+    private String getDate(long milliSeconds) {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(milliSeconds);
+        return formatter.format(calendar.getTime());
     }
 }
