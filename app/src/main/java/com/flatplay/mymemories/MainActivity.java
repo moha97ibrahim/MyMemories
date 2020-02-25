444713588761ae702a122cc9e0fd6140d7328d84
moha97brahim <moha97ibrahim@gmail.com>
2020-02-25 23:33:30 +0530

    remainder_Delete

--- app/src/main/java/com/flatplay/mymemories/MainActivity.java
+++ app/src/main/java/com/flatplay/mymemories/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
-
-import com.flatplay.mymemories.utility.NotifyService;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,23 +29,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-
-        setNotification();
-    }
-
-    private void setNotification() {
-        Intent myIntent = new Intent(MainActivity.this, NotifyService.class);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        PendingIntent pendingIntent = PendingIntent.getService(this, 0, myIntent, 0);
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.SECOND, 0);
-        calendar.set(Calendar.MINUTE, 14);
-        calendar.set(Calendar.HOUR_OF_DAY, 17);
-        calendar.set(Calendar.AM_PM, Calendar.PM);
-        calendar.add(Calendar.DAY_OF_MONTH, 1);
-
-        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 1000*60*60*24 , pendingIntent);
 
     }
 
--- app/src/main/java/com/flatplay/mymemories/ui/more/MoreFragment.java
+++ app/src/main/java/com/flatplay/mymemories/ui/more/MoreFragment.java
@@ -1,9 +1,13 @@
 package com.flatplay.mymemories.ui.more;
 
 import android.app.AlarmManager;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +21,8 @@ import android.widget.ToggleButton;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -24,9 +30,6 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.flatplay.mymemories.R;
 import com.flatplay.mymemories.ui.SplashAndLogIn.SplashScreenActivity;
-import com.flatplay.mymemories.utility.AlarmReceiver;
-import com.flatplay.mymemories.utility.NotifyService;
-import com.flatplay.mymemories.utility.TimePickerFragment;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -34,21 +37,12 @@ import java.util.Calendar;
 
 import static android.content.Context.ALARM_SERVICE;
 
-public class MoreFragment extends Fragment implements TimePickerDialog.OnTimeSetListener {
+public class MoreFragment extends Fragment {
 
     private FirebaseAuth firebaseAuth;
     private FirebaseUser firebaseUser;
-    private TextView logoutBtn, timeToRemind, alarmTextView;
-    private AlarmManager alarmManager;
-    private PendingIntent pendingIntent;
-    private TimePicker alarmTimePicker;
-    //private static AlarmActivity inst;
-    private Switch remindMeOnOff;
-    private Calendar calendar;
-    private int currentHour, currentMinute;
-    private String amPm;
-    private TimePickerDialog timePickerDialog;
-    private int hour,min;
+    private TextView logoutBtn, timeToRemind;
+    private String CHANNEL_ID = "id";
 
 
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -60,56 +54,18 @@ public class MoreFragment extends Fragment implements TimePickerDialog.OnTimeSet
 
 
         //=====================================================================================================================================
-
-        //=====================================================================================================================================
         timeToRemind = root.findViewById(R.id.time_to_remind);
         timeToRemind.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                calendar = Calendar.getInstance();
-                currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-                currentMinute = calendar.get(Calendar.MINUTE);
 
-                timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minutes) {
-                        if (hourOfDay >= 12) {
-                            amPm = "PM";
-                        } else {
-                            amPm = "AM";
-                        }
-                        timeToRemind.setText(String.format("%02d:%02d", hourOfDay, minutes) + amPm);
-                        hour = hourOfDay;
-                        min = minutes;
-                    }
-                }, currentHour, currentMinute, false);
-
-                timePickerDialog.show();
-                Log.d("MyActivity", "Alarm On");
-                Calendar calendar = Calendar.getInstance();
-                calendar.set(Calendar.HOUR_OF_DAY, hour);
-                calendar.set(Calendar.MINUTE,min );
-                Intent myIntent = new Intent(getActivity(), AlarmReceiver.class);
-                pendingIntent = PendingIntent.getBroadcast(getActivity(), 0, myIntent, 0);
-                alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
             }
         });
 
 
-//        alarmManager = (AlarmManager) getActivity().getSystemService(ALARM_SERVICE);
-//        remindMeOnOff = root.findViewById(R.id.remind_Switch);
-//        remindMeOnOff.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-//            @Override
-//            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-//                if (isChecked) {
-//
-//                } else {
-//                    alarmManager.cancel(pendingIntent);
-//                    setAlarmText("");
-//                    Log.d("MyActivity", "Alarm Off");
-//                }
-//            }
-//        });
+
+
+
 
 
         logoutBtn = root.findViewById(R.id.logOut_btn);
@@ -127,8 +83,7 @@ public class MoreFragment extends Fragment implements TimePickerDialog.OnTimeSet
         return root;
     }
 
-    @Override
-    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        timeToRemind.setText("Hour: " + hourOfDay + " Minute: " + minute);
+
     }
+
 }
